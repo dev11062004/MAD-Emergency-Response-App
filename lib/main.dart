73725c132a719a4b9254515e1bbc45b367f5fee6
dev11062004 <dev11062004@users.noreply.github.com>
2026-05-06 12:04:57 +0530

    chore: project initialization and core infrastructure
    - Initialized Flutter project with core dependencies (GetX, Firebase, Provider, Hive)
    - Configured Firebase options and cross-platform initialization
    - Established base architecture and directory structure
    - Configured app routing and theme tokens

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:public_emergency_app/Features/Response%20Screen/emergencies_screen.dart';
+// import 'package:permission_handler/permission_handler.dart';
+import 'Common Widgets/Onboarding.dart';
+import 'Features/User/Screens/SignUp/verify_email_page.dart';
+import 'firebase_options.dart';
+import 'package:provider/provider.dart';
+import 'package:public_emergency_app/mad_exam/providers/incident_provider.dart';
+import 'package:public_emergency_app/mad_exam/services/hive_service.dart';void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final hiveService = HiveService();
+  await hiveService.init();
+
+  runApp(MyApp(hiveService: hiveService));
+}
+
+class MyApp extends StatelessWidget {
+  final HiveService hiveService;
+  const MyApp({super.key, required this.hiveService});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var user = FirebaseAuth.instance.currentUser;
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => IncidentProvider(hiveService)),
+      ],
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: user == null ? const OnBoardingScreen() : const VerifyEmailPage(),
+        // home:const EmergenciesScreen(),
+      ),
+    );
+  }
+}
